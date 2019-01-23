80cb3c84c7f15bd90f4527a722ebaa0841d55658
David Pietrusa <dpietrusa@Davids-MacBook-Pro.local>
2019-01-23 11:45:49 -0500

    array inputs complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,91 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Declare an array named dailyAverageTemp
+        //
+        //• Instruct the user to input daily average temperatures (in
+        //degrees Fahrenheit) for 7 days
+        //
+        //• Each time the user inputs a value, add it to your
+        //dailyAverageTemp array
+
+
+    Scanner userInput = new Scanner(System.in)  ;
+
+    double[] dailyAverageTemp = new double[6];
+
+
+
+        //day 1
+        System.out.println("Enter an average temperature in degrees Fahrenheit for day 1");
+        double avgTemp0 = userInput.nextDouble();
+
+        dailyAverageTemp[0] = avgTemp0;
+
+
+        //day 2
+        System.out.println("Enter an average temperature in degrees Fahrenheit for day 2");
+        double avgTemp1 = userInput.nextDouble();
+
+        dailyAverageTemp[1] = avgTemp1;
+
+
+        //day 3
+        System.out.println("Enter an average temperature in degrees Fahrenheit for day 3");
+        double avgTemp2 = userInput.nextDouble();
+
+        dailyAverageTemp[2] = avgTemp2;
+
+
+        //day 4
+        System.out.println("Enter an average temperature in degrees Fahrenheit for day 4");
+        double avgTemp3 = userInput.nextDouble();
+
+        dailyAverageTemp[3] = avgTemp3;
+
+
+        //day 5
+        System.out.println("Enter an average temperature in degrees Fahrenheit for day 5");
+        double avgTemp4 = userInput.nextDouble();
+
+        dailyAverageTemp[4] = avgTemp4;
+
+
+        //day 6
+        System.out.println("Enter an average temperature in degrees Fahrenheit for day 6");
+        double avgTemp5 = userInput.nextDouble();
+
+        dailyAverageTemp[5] = avgTemp5;
+
+
+        //day 7
+        System.out.println("Enter an average temperature in degrees Fahrenheit for day 7");
+        double avgTemp6 = userInput.nextDouble();
+
+        dailyAverageTemp[6] = avgTemp6;
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
